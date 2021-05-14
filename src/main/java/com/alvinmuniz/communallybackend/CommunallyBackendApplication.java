500b7ec84409d655aa36e4ecb47a0c27a18b456a
Alvin Muniz <muniz.alvin@gmail.com>
2021-05-14 14:24:46 -0500

    initial commit

---
+++ src/main/java/com/alvinmuniz/communallybackend/CommunallyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alvinmuniz.communallybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunallyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunallyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alvinmuniz/communallybackend/CommunallyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvinmuniz.communallybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunallyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
